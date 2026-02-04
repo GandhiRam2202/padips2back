1c7fc1951f3424c8be96a66b6ebc477f3aa7e4a1
GandhiRam2202 <gandhiramparthi.com>
2026-02-05 01:32:58 +0530

    update

--- models/User.js
+++ models/User.js
@@ -7,6 +7,7 @@ const userSchema = new mongoose.Schema({
   dob: String,
   otp: String,
   otpExpires: Date,
+  role: { type: String, default: "user" }, // Added role field
 });
 
 export default mongoose.model("User", userSchema);
